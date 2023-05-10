64a565c92214d4c042dd0ef6b24699593d15e2f5
midasdev711 <midasdev711@gmail.com>
2023-05-10 11:48:11 -0700

    highlight link

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 						<Link
 							key={item.href}
 							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+							className="text-md duration-500 text-white hover:text-zinc-300"
 						>
 							{item.name}
 						</Link>
